52e46a6aa0f3f3a6b0f0eca2c6ad9d7a211553ad
vspominay <nemogyvhs@gmail.com>
2022-04-03 14:07:13 +0300

    set default cities for finding

---
+++ src/app/components/search/defaultCities.ts
@@ -0,0 +1,210 @@
+export const defaultCities = [
+            {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            33.416839,
+            49.078092
+          ]
+        },
+        "properties": {
+          "id": "101752881",
+          "gid": "whosonfirst:locality:101752881",
+          "layer": "locality",
+          "source": "whosonfirst",
+          "source_id": "101752881",
+          "name": "Kremenchuk",
+          "confidence": 1,
+          "match_type": "exact",
+          "accuracy": "centroid",
+          "country": "Ukraine",
+          "country_gid": "whosonfirst:country:85633805",
+          "country_a": "UKR",
+          "region": "Poltava",
+          "region_gid": "whosonfirst:region:85688859",
+          "region_a": "MY",
+          "county": "Kremenchutska Raion",
+          "county_gid": "whosonfirst:county:1108815839",
+          "locality": "Kremenchuk",
+          "locality_gid": "whosonfirst:locality:101752881",
+          "continent": "Europe",
+          "continent_gid": "whosonfirst:continent:102191581",
+          "label": "Kremenchuk, Ukraine"
+        },
+        "bbox": [
+          33.376996,
+          49.0098615,
+          33.491208,
+          49.149719
+        ]
+      },
+
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            19.453413,
+            50.160494
+          ]
+        },
+        "properties": {
+          "id": "101841591",
+          "gid": "whosonfirst:locality:101841591",
+          "layer": "locality",
+          "source": "whosonfirst",
+          "source_id": "101841591",
+          "name": "Trzebinia",
+          "confidence": 1,
+          "match_type": "exact",
+          "accuracy": "centroid",
+          "country": "Poland",
+          "country_gid": "whosonfirst:country:85633723",
+          "country_a": "POL",
+          "region": "Lesser Poland Voivodeship",
+          "region_gid": "whosonfirst:region:85687291",
+          "region_a": "MA",
+          "county": "Chrzanów",
+          "county_gid": "whosonfirst:county:102079495",
+          "localadmin": "Gmina Trzebinia",
+          "localadmin_gid": "whosonfirst:localadmin:1125326729",
+          "locality": "Trzebinia",
+          "locality_gid": "whosonfirst:locality:101841591",
+          "continent": "Europe",
+          "continent_gid": "whosonfirst:continent:102191581",
+          "label": "Trzebinia, Poland"
+        },
+        "bbox": [
+          19.38743195,
+          50.132440801,
+          19.488750563,
+          50.226574424
+        ]
+      },
+
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            24.031393,
+            49.841559
+          ]
+        },
+        "properties": {
+          "id": "101752483",
+          "gid": "whosonfirst:locality:101752483",
+          "layer": "locality",
+          "source": "whosonfirst",
+          "source_id": "101752483",
+          "name": "Lviv",
+          "confidence": 1,
+          "match_type": "exact",
+          "accuracy": "centroid",
+          "country": "Ukraine",
+          "country_gid": "whosonfirst:country:85633805",
+          "country_a": "UKR",
+          "region": "Lviv",
+          "region_gid": "whosonfirst:region:85688887",
+          "region_a": "LV",
+          "county": "L'vivs'ka",
+          "county_gid": "whosonfirst:county:1108730647",
+          "locality": "Lviv",
+          "locality_gid": "whosonfirst:locality:101752483",
+          "continent": "Europe",
+          "continent_gid": "whosonfirst:continent:102191581",
+          "label": "Lviv, Ukraine"
+        },
+        "bbox": [
+          23.91113,
+          49.769041,
+          24.1209055,
+          49.891745
+        ]
+      },
+
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            30.431971,
+            50.469863
+          ]
+        },
+        "properties": {
+          "id": "101752489",
+          "gid": "whosonfirst:locality:101752489",
+          "layer": "locality",
+          "source": "whosonfirst",
+          "source_id": "101752489",
+          "name": "Kiev City",
+          "confidence": 1,
+          "match_type": "exact",
+          "accuracy": "centroid",
+          "country": "Ukraine",
+          "country_gid": "whosonfirst:country:85633805",
+          "country_a": "UKR",
+          "region": "Kiev",
+          "region_gid": "whosonfirst:region:85688801",
+          "region_a": "KV",
+          "county": "Pechers'kyi",
+          "county_gid": "whosonfirst:county:1108730705",
+          "locality": "Kiev City",
+          "locality_gid": "whosonfirst:locality:101752489",
+          "continent": "Europe",
+          "continent_gid": "whosonfirst:continent:102191581",
+          "label": "Kiev City, Ukraine"
+        },
+        "bbox": [
+          30.23709,
+          50.21354,
+          30.828148,
+          50.588962
+        ]
+      },
+
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            36.230969,
+            49.978037
+          ]
+        },
+        "properties": {
+          "id": "101752963",
+          "gid": "whosonfirst:locality:101752963",
+          "layer": "locality",
+          "source": "whosonfirst",
+          "source_id": "101752963",
+          "name": "Kharkiv",
+          "confidence": 1,
+          "match_type": "exact",
+          "accuracy": "centroid",
+          "country": "Ukraine",
+          "country_gid": "whosonfirst:country:85633805",
+          "country_a": "UKR",
+          "region": "Kharkiv",
+          "region_gid": "whosonfirst:region:85688873",
+          "region_a": "KK",
+          "county": "Kharkivs'ka",
+          "county_gid": "whosonfirst:county:1108730667",
+          "locality": "Kharkiv",
+          "locality_gid": "whosonfirst:locality:101752963",
+          "continent": "Europe",
+          "continent_gid": "whosonfirst:continent:102191581",
+          "label": "Kharkiv, Ukraine"
+        },
+        "bbox": [
+          36.130946,
+          49.888223,
+          36.4526005,
+          50.095901
+        ]
+      }
+
+
+]
